364caa8b161a2c8699790219a34c151d83938e45
deniskhamenka <deniskhamenka@outlook.com>
2025-07-31 14:21:28 +0200

    3rd task is done

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,20 @@
   преобразовываясь в number
 
 */
+
+  const minAge = 18;
+  const maxAge = 60;
+  let age = "60"; // 10, 17, 18, 19, 59, 60, 61
+  age = Number(age); // 3rd task?
+
+  if (typeof age != "number"){
+   console.log('Incorrect data type')
+  } else if (age < minAge){
+    console.log("You don't have access cause your age is " + age + " It's less then 18")
+  } else if (age >= minAge && age < maxAge){
+    console.log("Welcome  !")
+  } else if (age > maxAge){
+    console.log("Keep calm and look Culture channel")
+  } else {
+    console.log("Technical work")
+  };
